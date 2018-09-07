6f39219b11e8a8bc58ba081af2182f5894314657
selenax <selenawenxu@gmail.com>
2018-09-07 16:02:16 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi!</h1>
+    <p>My name is Selena.</p>
+    <p>Looking forward to meeting everyone!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
